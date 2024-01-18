9721d9bb87df18a596f30796114cfae2a1a548a2
Ethan Whittle <ethanwhittle@hotmail.co.uk>
2024-01-18 23:30:09 +0000

    Added Request and Response Model Mappings

---
+++ src/LineTen.TechnicalTask.Service.Domain.Tests/Mappings/RequestModelProfileTests.cs
@@ -0,0 +1,197 @@
+using AutoMapper;
+using LineTen.TechnicalTask.Domain.Enums;
+using LineTen.TechnicalTask.Domain.Models;
+using LineTen.TechnicalTask.Service.Domain.Models;
+
+namespace LineTen.TechnicalTask.Service.Domain.Tests.Mappings
+{
+    public class RequestModelProfile : Profile
+    {
+        public RequestModelProfile()
+        {
+            CreateMap<AddCustomerRequest, Customer>()
+                .ForMember(d => d.Id, opt => opt.Ignore());
+
+            CreateMap<UpdateCustomerRequest, Customer>();
+
+            CreateMap<AddProductRequest, Product>()
+                .ForMember(d => d.Id, opt => opt.Ignore());
+
+            CreateMap<UpdateProductRequest, Product>();
+
+            CreateMap<AddOrderRequest, Order>()
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.CreatedDate, opt => opt.Ignore())
+                .ForMember(d => d.UpdatedDate, opt => opt.Ignore());
+
+            CreateMap<UpdateOrderRequest, Order>()
+                .ForMember(d => d.CreatedDate, opt => opt.Ignore())
+                .ForMember(d => d.UpdatedDate, opt => opt.Ignore());
+        }
+    }
+
+    public class RequestModelProfileTests
+    {
+        private readonly MapperConfiguration _mapperConfiguration;
+        private readonly IMapper _mapper;
+
+        public RequestModelProfileTests()
+        {
+            _mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<RequestModelProfile>());
+            _mapper = _mapperConfiguration.CreateMapper();
+        }
+
+        [Fact]
+        public void CanConstruct()
+        {
+            // Act
+            var instance = new RequestModelProfile();
+
+            // Assert
+            instance.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ConfigurationIsValid()
+        {
+            // Assert
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void MapsAddCustomerRequestToCustomer()
+        {
+            // Arrange
+            var request = new AddCustomerRequest
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Phone = "+1-555-1234",
+                Email = "john.doe@example.com"
+            };
+
+            // Act
+            var mapped = _mapper.Map<AddCustomerRequest, Customer>(request);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<Customer>();
+            mapped.FirstName.Should().Be(request.FirstName);
+            mapped.LastName.Should().Be(request.LastName);
+            mapped.Phone.Should().Be(request.Phone);
+            mapped.Email.Should().Be(request.Email);
+        }
+
+        [Fact]
+        public void MapsUpdateCustomerRequestToCustomer()
+        {
+            // Arrange
+            var request = new UpdateCustomerRequest
+            {
+                Id = 1,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Phone = "+1-555-5678",
+                Email = "jane.doe@example.com"
+            };
+
+            // Act
+            var mapped = _mapper.Map<UpdateCustomerRequest, Customer>(request);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<Customer>();
+            mapped.Id.Should().Be(request.Id);
+            mapped.FirstName.Should().Be(request.FirstName);
+            mapped.LastName.Should().Be(request.LastName);
+            mapped.Phone.Should().Be(request.Phone);
+            mapped.Email.Should().Be(request.Email);
+        }
+
+        [Fact]
+        public void MapsAddProductRequestToProduct()
+        {
+            // Arrange
+            var request = new AddProductRequest
+            {
+                Name = "Product Name",
+                Description = "Product Description",
+                SKU = "ABC123"
+            };
+
+            // Act
+            var mapped = _mapper.Map<AddProductRequest, Product>(request);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<Product>();
+            mapped.Name.Should().Be(request.Name);
+            mapped.Description.Should().Be(request.Description);
+            mapped.SKU.Should().Be(request.SKU);
+        }
+
+        [Fact]
+        public void MapsUpdateProductRequestToProduct()
+        {
+            // Arrange
+            var request = new UpdateProductRequest
+            {
+                Id = 1,
+                Name = "Updated Product Name",
+                Description = "Updated Product Description",
+                SKU = "XYZ789"
+            };
+
+            // Act
+            var mapped = _mapper.Map<UpdateProductRequest, Product>(request);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<Product>();
+            mapped.Id.Should().Be(request.Id);
+            mapped.Name.Should().Be(request.Name);
+            mapped.Description.Should().Be(request.Description);
+            mapped.SKU.Should().Be(request.SKU);
+        }
+
+        [Fact]
+        public void MapsAddOrderRequestToOrder()
+        {
+            // Arrange
+            var request = new AddOrderRequest
+            {
+                ProductId = 1,
+                CustomerId = 2,
+                Status = OrderStatus.Completed
+            };
+
+            // Act
+            var mapped = _mapper.Map<AddOrderRequest, Order>(request);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<Order>();
+            mapped.ProductId.Should().Be(request.ProductId);
+            mapped.CustomerId.Should().Be(request.CustomerId);
+            mapped.Status.Should().Be(request.Status);
+        }
+
+        [Fact]
+        public void MapsUpdateOrderRequestToOrder()
+        {
+            // Arrange
+            var request = new UpdateOrderRequest
+            {
+                Id = 1,
+                ProductId = 3,
+                CustomerId = 4,
+                Status = OrderStatus.InProgress
+            };
+
+            // Act
+            var mapped = _mapper.Map<UpdateOrderRequest, Order>(request);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<Order>();
+            mapped.Id.Should().Be(request.Id);
+            mapped.ProductId.Should().Be(request.ProductId);
+            mapped.CustomerId.Should().Be(request.CustomerId);
+            mapped.Status.Should().Be(request.Status);
+        }
+    }
+}
---
+++ src/LineTen.TechnicalTask.Service.Domain.Tests/Mappings/ResponseModelProfileTests.cs
@@ -0,0 +1,121 @@
+using AutoMapper;
+using LineTen.TechnicalTask.Domain.Enums;
+using LineTen.TechnicalTask.Domain.Models;
+using LineTen.TechnicalTask.Service.Domain.Models;
+
+namespace LineTen.TechnicalTask.Service.Domain.Tests.Mappings
+{
+    public class ResponseModelProfile : Profile
+    {
+        public ResponseModelProfile()
+        {
+            CreateMap<Customer, CustomerResponse>();
+            CreateMap<Product, ProductResponse>();
+            CreateMap<Order, OrderResponse>();
+        }
+    }
+
+    public class ResponseModelProfileTests
+    {
+        private readonly MapperConfiguration _mapperConfiguration;
+        private readonly IMapper _mapper;
+
+        public ResponseModelProfileTests()
+        {
+            _mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile<ResponseModelProfile>());
+            _mapper = _mapperConfiguration.CreateMapper();
+        }
+
+        [Fact]
+        public void CanConstruct()
+        {
+            // Act
+            var instance = new ResponseModelProfile();
+
+            // Assert
+            instance.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ConfigurationIsValid()
+        {
+            // Assert
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void MapsCustomerToCustomerResponse()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Phone = "+1-555-1234",
+                Email = "john.doe@example.com"
+            };
+
+            // Act
+            var mapped = _mapper.Map<Customer, CustomerResponse>(customer);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<CustomerResponse>();
+            mapped.Id.Should().Be(customer.Id);
+            mapped.FirstName.Should().Be(customer.FirstName);
+            mapped.LastName.Should().Be(customer.LastName);
+            mapped.Phone.Should().Be(customer.Phone);
+            mapped.Email.Should().Be(customer.Email);
+        }
+
+        [Fact]
+        public void MapsProductToProductResponse()
+        {
+            // Arrange
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Product Name",
+                Description = "Product Description",
+                SKU = "ABC123"
+            };
+
+            // Act
+            var mapped = _mapper.Map<Product, ProductResponse>(product);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<ProductResponse>();
+            mapped.Id.Should().Be(product.Id);
+            mapped.Name.Should().Be(product.Name);
+            mapped.Description.Should().Be(product.Description);
+            mapped.SKU.Should().Be(product.SKU);
+        }
+
+        [Fact]
+        public void MapsOrderToOrderResponse()
+        {
+            // Arrange
+            var order = new Order
+            {
+                Id = 1,
+                ProductId = 2,
+                CustomerId = 3,
+                Status = OrderStatus.Completed,
+                CreatedDate = DateTime.Now.AddDays(-2),
+                UpdatedDate = DateTime.Now.AddDays(-1)
+            };
+
+            // Act
+            var mapped = _mapper.Map<Order, OrderResponse>(order);
+
+            // Assert
+            mapped.Should().NotBeNull().And.BeOfType<OrderResponse>();
+            mapped.Id.Should().Be(order.Id);
+            mapped.ProductId.Should().Be(order.ProductId);
+            mapped.CustomerId.Should().Be(order.CustomerId);
+            mapped.Status.Should().Be(order.Status);
+            mapped.CreatedDate.Should().Be(order.CreatedDate);
+            mapped.UpdatedDate.Should().Be(order.UpdatedDate);
+        }
+    }
+}
